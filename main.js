dfd68b9714f78279431f7b99dbd592b7211a65e7
mkjeung <mkjeung@uchicago.edu>
2025-12-05 12:36:59 -0600

    a

--- main.js
+++ main.js
@@ -113,17 +113,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const min = 0;
     document.addEventListener('mousemove', function(e) {
         const isOverBox = e.target.closest('.portfolio-box') !== null || e.target.closest('.nav-box') !== null || e.target.closest('.scroll-btn') !== null;
-        const isOverTitle = e.target.closest('#intro') !== null || e.target.closest('#portfolio') !== null || e.target.closest('#contact') !== null || e.target.closest('.header-text') !== null;
+        const isOverLink = e.target.closest('a') !== null;
         offset_x = offset_x + Math.random() * (max - min) + min;
         offset_y = offset_y + Math.random() * (max - min) + min;
         offset_size = offset_size + Math.random() * (max - min) + min;
         if (isOverBox) {
             cursorGlow.style.display = 'none';
-        } else if(isOverTitle) {
+        } else if(isOverLink) {
             cursorGlow.style.display = 'block';
-            cursorGlow.style.width = `${300}px`;
-            cursorGlow.style.height = `${300}px`;
-            cursorGlow.style.transform = `translate3d(${e.pageX-150}px, ${e.pageY -150}px, 0)`;
+            cursorGlow.style.width = `${100}px`;
+            cursorGlow.style.height = `${100}px`;
+            cursorGlow.style.transform = `translate3d(${e.pageX-50}px, ${e.pageY -50}px, 0)`;
         } else {
             cursorGlow.style.display = 'none';
             cursorGlow.style.display = 'block';
